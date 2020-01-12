e71e0c7408bbcb50eea18ff789271e30f6d448b8
mk <yu7319944@gmail.com>
2020-01-12 10:11:40 +0000

    333

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -45,7 +45,7 @@ class LinebotController < ApplicationController
                 "明日の天気をお伝えします。\n明日は雨が降りそう...\n\n・天候: #{weather}\n・最高気温: #{maxtemp}°\n・最低気温: #{mintemp}°\n・降水確率: #{per12to18}％"
             else
               push =
-                "明日の天気をお伝えします。\n明日雨は降らなそうだぞ！\n\n・天候　#{weather}\n・最高気温: #{maxtemp}°\n・最低気温: #{mintemp}°\n・降水確率: #{per12to18}％"
+                "明日の天気をお伝えします。\n明日雨は降らなそうだぞ！\n\n・天候: #{weather}\n・最高気温: #{maxtemp}°\n・最低気温: #{mintemp}°\n・降水確率: #{per12to18}％"
             end
           when /.*(明後日|あさって).*/
             per06to12 = doc.elements[xpath + 'info[3]/rainfallchance/period[2]'].text
